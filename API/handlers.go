bb39f4f31f904066c736a340283b5c698decea89
Franklynoble <Franklynoblez@gmail.com>
2022-08-26 14:24:31 +0100

    complete handlers

--- API/handlers.go
+++ API/handlers.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
+	"strconv"
 	"sync"
 
 	"github.com/Franklynoble/todocli"
-	"golang.org/x/text/message"
 )
 
 var (
@@ -17,7 +19,7 @@ var (
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		replyError(w, r, http.StatusNotFound, "")
 		return
 
 	}
@@ -83,9 +85,9 @@ func todoRouter(todoFile string, l sync.Locker) http.HandlerFunc {
 			getOneHandler(w, r, list, id)
 
 		case http.MethodDelete:
-			deletHandler(w, r, list, id, todoFile)
+			deleteHandler(w, r, list, id, todoFile)
 		case http.MethodPatch:
-               pathHandler(w,r list, id, todoFile)
+			patchHandler(w, r, list, id, todoFile)
 		default:
 			message := "Method not supported"
 			replyError(w, r, http.StatusMethodNotAllowed, message)
@@ -95,42 +97,83 @@ func todoRouter(todoFile string, l sync.Locker) http.HandlerFunc {
 
 }
 
-func getAllHandler(w http.ResponseWriter, r *http.Request, list *todocli.list) {
-resp := &todoResponse {
-	Results: *list,
+func getAllHandler(w http.ResponseWriter, r *http.Request, list *todocli.List) {
+	resp := &todoResponse{
+		Results: *list,
 	}
-	replyJSONContent(w,r, http.StatusOk, resp)	
+	replyJSONContent(w, r, http.StatusOK, resp)
 }
 
+func getOneHandler(w http.ResponseWriter, r *http.Request, list *todocli.List, id int) {
 
-func  getOneHandler(w http.ResponseWriter, r *http.Request, list *todocli.list, id int) {
-
-  resp := &todoResponse {
-	Results: (*list)[id-1: id],
-
-  }
-  replyJSONContent(w, r, http.StatusOk, resp)
+	resp := &todoResponse{
+		Results: (*list)[id-1 : id],
+	}
+	replyJSONContent(w, r, http.StatusOK, resp)
 
 }
 
-func deleteHandler(w http.ResponsWriter, r *http.Request, list *todocli.list, id int, todtodoFile) {
+func deleteHandler(w http.ResponseWriter, r *http.Request, list *todocli.List, id int, todoFile string) {
 	list.Delete(id)
 
 	if err := list.Save(todoFile); err != nil {
 		replyError(w, r, http.StatusInternalServerError, err.Error())
-	
-	 return
+
+		return
 	}
-	replyTextContent(w,r , http.StatusaNoContent, "")
+	replyTextContent(w, r, http.StatusNoContent, "")
 }
 
-
-func patchHandler(w http.ResponseWriter, r *http.Request, list *todocli.Llist, id int, todoFile string) {
+func patchHandler(w http.ResponseWriter, r *http.Request, list *todocli.List, id int, todoFile string) {
 	q := r.URL.Query()
 
 	if _, ok := q["complete"]; !ok {
 		message := "Missing query param 'complete'"
-		replyError(w,r , http.StatusBadRequest, message)
-	   return
+		replyError(w, r, http.StatusBadRequest, message)
+		return
 	}
+	list.Complete(id)
+	if err := list.Save(todoFile); err != nil {
+		replyError(w, r, http.StatusInternalServerError, err.Error())
+		return
+	}
+	replyTextContent(w, r, http.StatusNoContent, "")
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request, list *todocli.List, todoFile string) {
+	item := struct {
+		Task string `json:"task"`
+	}{}
+	//  representing the task name to include in the list. It decodes the JSON into
+	// an anonymous struct and uses it to add the new item. It replies with the HTTP
+	// Status Created if successful or an appropriate error status if errors occur
+	if err := json.NewDecoder(r.Body).Decode(&item); err != nil {
+		message := fmt.Sprintf("Invalid JSON: %s", err)
+		replyError(w, r, http.StatusBadRequest, message)
+		return
+	}
+
+	list.Add(item.Task)
+	if err := list.Save(todoFile); err != nil {
+		replyError(w, r, http.StatusInternalServerError, err.Error())
+		return
+	}
+	replyTextContent(w, r, http.StatusCreated, "")
+}
+
+func validateID(path string, list *todocli.List) (int, error) {
+	//convert string id to int using strconv.Atoi
+	id, err := strconv.Atoi(path)
+
+	if err != nil {
+		return 0, fmt.Errorf("%w: Invalid ID: %s", ErrInvalidData, err)
+	}
+	if id < 1 {
+		return 0, fmt.Errorf("%W, invalid ID: Less than one", ErrInvalidData)
+	}
+	if id > len(*list) {
+		return id, fmt.Errorf("%w: ID %d not found", ErrNoteFound, id)
+	}
+
+	return id, nil
 }
